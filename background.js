5b1b21244a5977434032249b724849af53f811bc
Aayush Panda <aayush.vinayak@gmail.com>
2021-05-28 09:26:51 -0400

    added css and testing features

--- background.js
+++ background.js
@@ -7,11 +7,15 @@ function onError(e) {
 }
 
 var bibliography = "";
+var citations = [];
 
 function add_citation() {   // Cite current url (where extension was activated)
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        let citation = create_citation(url);
         bibliography += "\n";
-        bibliography += create_citation(tabs[0].url);
+        bibliography += citation;
+        citations.push()
     });
 }
 
@@ -19,15 +23,20 @@ function clear_bibliography() {    // Clear all data in bibliography
     bibliography = ""
 }
 
-function paste_bibliography() {    // Copy bibliography to clipboard
+function paste_bibliography() {    // Copy bibliography to user clipboard
     navigator.clipboard.writeText(bibliography).then()
 }
 
-function copy_link_from_clipboard() {   // Add citation to bibliography using clipboard data
+function paste_citation() {    // Copy latest citation to user clipboard
+    navigator.clipboard.writeText(citations[-1]).then()
+}
+
+function copy_link_from_clipboard() {   // Add citation to bibliography using user clipboard data
     bibliography += "\n";
     bibliography += create_citation(navigator.clipboard.readText())
 }
 
 function create_citation(link="") {    // Function to generate citation
-
+    document.getElementById('url_disp').value = link;   // ONLY FOR TESTING
+    return link
 }
