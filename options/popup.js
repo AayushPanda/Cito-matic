3bd2b434d43bdcf10ada7c0d392725ffa56a28d6
Aayush Panda <aayush.vinayak@gmail.com>
2021-05-29 13:34:34 -0400

    fixing persistence

--- options/popup.js
+++ options/popup.js
@@ -45,12 +45,12 @@ if(getData("Citations") === undefined){
 // Functions to interact with synced data
 function getData(key="") {
     if(syncData){
-        chrome.storage.sync.get([key], (result) => {
+        chrome.storage.sync.get([key], function(result) {
             console.log(result.values);
             return result.values;
         });
     } else {
-        chrome.storage.local.get([key], (result) => {
+        chrome.storage.local.get([key], function(result) {
             return result.values;
         });
     }
@@ -58,11 +58,11 @@ function getData(key="") {
 
 function setData(target_key="", value) {
     if(syncData){
-        chrome.storage.sync.set({target_key: value}, () => {
+        chrome.storage.sync.set({target_key: value}, function() {
             console.log('Value ' + target_key + ' is set to ' + value);
         });
     } else {
-        chrome.storage.local.set({target_key: value}, () => {
+        chrome.storage.local.set({target_key: value}, function() {
             console.log('Value ' + target_key + ' is set to ' + value);
         });
     }
