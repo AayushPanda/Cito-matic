1aee5c237de5b0f0187417e8cbc8e4914b596e0e
Yelll0 <zhang.carl.x@gmail.com>
2021-05-29 19:59:24 -0400

    Functional switch and background script

--- popup/popup.js
+++ popup/popup.js
@@ -3,62 +3,26 @@ function onError(e) {
     console.error(e);
 }
 
-// Declaring UI elements
-let add_cit = document.getElementById('add_cit');
-let copy_bib = document.getElementById('copy_bib');
-let clear_bib = document.getElementById('clear_bib');
-// bibliography = "References \n";
-
 // Adding listeners to UI elements
-add_cit.addEventListener('click', () => {
-    addCitation();
-});
-copy_bib.addEventListener('click', () => {
+document.getElementById('copy_bib').addEventListener('click', () => {
     copyBib();
 });
-clear_bib.addEventListener('click', () => {
+document.getElementById('clear_bib').addEventListener('click', () => {
     clearBib();
 });
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.set({"Bibliography": ["A"]}, () => {
-        console.log("Bibliography created");
+document.getElementById('switch').addEventListener('click', () => {
+    chrome.storage.local.get(["Activated"], (result) => {
+        chrome.storage.local.set({"Activated": !result.Activated}, () => {});
     });
 });
 
 chrome.storage.local.get(["Bibliography"], (result) => {
     document.getElementById('url_disp').value = formatBib(result.Bibliography);
-/*    
-    bib = result.Bibliography;
-
-    bib.forEach(function (value) {
-        bibliography += value;
-        bibliography += " \n";
-    });
-
-    document.getElementById('url_disp').value = bibliography;
-*/
 });
 
-function addCitation(url) {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-        let url = tabs[0].url;
-        chrome.storage.local.get(["Bibliography"], (result) => {
-            let bib;
-            if(result.Bibliography === undefined){
-                bib = [];
-                /* bib = ["References"]; */
-            } else {
-                bib = result.Bibliography;
-            }
-            if(!bib.includes(url)){
-                bib.push(url);
-            }
-            chrome.storage.local.set({"Bibliography": bib}, () => {});
-            document.getElementById('url_disp').value = formatBib(bib);
-        });
-    });
-}
+chrome.storage.local.get(["Activated"], (result) => {
+    document.getElementById('switch').checked = result.Activated;
+});
 
 function copyBib() {
     chrome.storage.local.get(["Bibliography"], (result) => {
@@ -70,11 +34,6 @@ function clearBib() {
     chrome.storage.local.set({"Bibliography": []}, () => {
         console.log("Bibliography cleared");
         document.getElementById('url_disp').value = "";
-/*
-        bib = ["References"];
-        bibliography = "References \n";
-        document.getElementById('url_disp').value = bibliography;
-*/
     });
 }
 
