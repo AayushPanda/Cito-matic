5ecd443098625969128d63ce93301e7f30dc748a
Aayush Panda <aayush.vinayak@gmail.com>
2021-05-29 13:45:44 -0400

    fixing persistence

--- popup/popup.js
+++ popup/popup.js
@@ -26,6 +26,32 @@ clear_bib.addEventListener('click', function () {
 // Global variables
 let syncData = true;    // TODO Add switch in HTML to change this variable's value
 
+// Functions to interact with synced data
+function getData(key="") {
+    if(syncData){
+        chrome.storage.sync.get([key], function(result) {
+            console.log(result.values);
+            return result.values;
+        });
+    } else {
+        chrome.storage.local.get([key], function(result) {
+            return result.values;
+        });
+    }
+}
+
+function setData(target_key="", value) {
+    if(syncData){
+        chrome.storage.sync.set({target_key: value}, function() {
+            console.log('Value ' + target_key + ' is set to ' + value);
+        });
+    } else {
+        chrome.storage.local.set({target_key: value}, function() {
+            console.log('Value ' + target_key + ' is set to ' + value);
+        });
+    }
+}
+
 // Getting saved citation data
 if(getData("Bibliography") === undefined){
     bibliography = "References \n";
