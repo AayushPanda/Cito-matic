8ec647e71d10022934ece07aac5cb41155d8a5cd
Aayush Panda <aayush.vinayak@gmail.com>
2021-05-29 12:49:58 -0400

    fixing persistent bibliography

--- options/popup.js
+++ options/popup.js
@@ -71,7 +71,7 @@ function setData(target_key="", value) {
 // User-interacted functions
 function add_citation() {   // Cite current url (where extension was activated)
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
+        let url = tabs[tabs.length-1].url;
         let citation = create_citation(url);
         bibliography += citation;
         bibliography += "\n";
